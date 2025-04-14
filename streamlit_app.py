9c78e66321c4f539798d56da9923a3dc881ab732
IntendedEvents <info@intendedevents.ca>
2025-04-14 12:05:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -168,7 +168,7 @@ for tier, weights in priority_weights.items():
         w = weights[category_priorities[cat]]
         value = (g * w[0] + b * w[1] + bst * w[2]) * scaling_factor
 
-        if cat in category_minimums and cat in included_categories:
+if cat in category_minimums and cat in included_categories:
     value = max(value, category_minimums[cat])
 
         if cat == "Decor & Rentals (Furniture, decor, tent, etc.)" and tent_needed:
