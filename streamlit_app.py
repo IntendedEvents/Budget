f08351fbf9906e92332058287298089523afa09e
IntendedEvents <info@intendedevents.ca>
2025-04-14 16:15:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,7 +183,7 @@ if tier == "Elevated":
 If this feels like a helpful starting point, amazing!  
 Take your results and review them with your wedding planner or someone with experience navigating local vendors and venues.
 
-If you’re planning a **Vancouver Island wedding**, this tool was created with *you* in mind — whether you're dreaming of forest elopements, coastal celebrations, or backyard parties with your people.
+If you're planning a **Vancouver Island wedding**, this tool was created with *you* in mind — whether you're dreaming of forest elopements, coastal celebrations, or backyard parties with your people.
 
 We’re cheering you on from here 💛
 
